166942c8285a1f998cfc790bee555c1b9a29cfec
tishparkinson <tish4575@gmail.com>
2017-02-11 22:00:50 -0700

    added budget page to show all transactions

---
+++ web/budgetSearch.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Scripture Resources</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/cs313-php.css">
+</head>
+<body>
+<h1>Budget Search</h1>
+<br />
+<?php
+include 'dbstuff.inc';
+
+
+
+foreach ($db->query('SELECT * from transactionsactions') as $row)
+{
+ print "<p><span id='transactionreference'>$row[1] $row[2] $row[3] $row[4]</p>\n\n";
+}
+?>
+<br />
+<hr>
+<br />
+</body>
+</html>
