06d04b59995ac5db11fe0b81c9b379430b708a01
tishparkinson <tish4575@gmail.com>
2017-02-18 08:17:01 -0700

    added edit option

--- web/budgetSearch.php
+++ web/budgetSearch.php
@@ -18,7 +18,7 @@ include 'dbstuff.inc';
 
 
 
-foreach ($db->query('SELECT * from transactions') as $row)
+foreach ($db->query('SELECT * from transactions') as $row) <input class="radio" type="radio" name="edit" value="edit"/> <label for="editpage">Edit</label>
 {
  print "<p><span id='transactionreference'>$row[4]<br> $row[1]<br> $row[2] </p>\n\n";
 }
@@ -47,6 +47,59 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <br />
 <hr>
 <br />
+<script type="text/javascript">
+    $(document).ready(function() {
+      $("input[name='edit']").click(function(){
+          if ($("input[name='edit']:checked").val() == 'edit') {
+              $( "#regdiv" ).hide();
+              $( "#editdiv").show();
+          }else {
+              $("#editdiv").hide();
+              $("#regdiv").show();
+          }
+      });
+
+        $('#login').submit(function(e){
+
+            e.preventDefault(); // Prevent Default Submission
+
+            $.ajax({
+                url: 'login-inc.php',
+                type: 'POST',
+                data: $(this).serialize() // it will serialize the form data
+            })
+                .done(function(data){
+                    if (data == 'valid') {
+                        window.location.href = 'budgetSearch.php'
+                    } else {
+                        $('#result').text(data);
+                    }
+
+                })
+                .fail(function(){
+                    alert('Ajax Submit Failed ...');
+                });
+        });
+
+        $('#reg').submit(function(e){
+
+            e.preventDefault(); // Prevent Default Submission
+
+            $.ajax({
+                url: 'dbstuff.inc',
+                type: 'POST',
+                data: $(this).serialize() // it will serialize the form data
+            })
+                .done(function(data){
+                    $('#result').text(data);
+                })
+                .fail(function(){
+                    alert('Ajax Submit Failed ...');
+                });
+        });
+    });
+</script>
+
 <a href="assignments.html">back to: Assignments Page</a>
 </body>
 </html>
